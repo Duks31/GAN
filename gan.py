e0309e83e0f69ab71153af86089700ffb0d6f239
Duks31 <chidubemjan31@gmail.com>
2024-03-25 17:46:55 +0100

    added missing import

--- gan.py
+++ gan.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torchvision.datasets as 
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+
 from torch.utils.tensorboard import SummaryWriter
 
-class Discriminator(nn.Module):
+class Discriminator(nn.Module): 
     def __init__(self, in_features):
-        super().__init__()
+        super().__init__()1
         self.disc = nn.Sequential(
             nn.Linear(in_features, 128),
             nn.LeakyReLU(0.1),
